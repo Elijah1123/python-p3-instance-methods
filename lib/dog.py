916beb48e10323f7c930b07b7d9c6ae19f90a225
Elijah1123 <elijahmzalendo659@gmail.com>
2025-08-23 00:37:16 +0300

    python

--- lib/dog.py
+++ lib/dog.py
@@ -8,4 +8,21 @@ class Dog:
     def sit(self):
         print("The dog is sitting.")
 
+    def eat(self):
+        print("The dog is eating.")
+
+    def playing(self):
+        print("The dog is playing")    
+ 
+fido = Dog()
+fido.bark()
+
+fido = Dog()
+fido.sit()
+
+fido = Dog() 
+fido.eat()
+
+fido = Dog()
+fido.playing()
     
